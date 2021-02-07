ffe2243fb45683ed22dd28ec6df592203af53cd8
Kawasaki Hirotsugu <kawa.ikkyou@gmail.com>
2021-02-08 07:39:42 +0900

    create react component

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Hello = (props) => {
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href="https://github.com/mluukkai">mluukkai</a>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
+  return (
+  <>
+    <h1>Greetings</h1>
+    <Hello name="Maya" age={26 + 10} />
+    <Hello name={name} age={age} />
+    <Footer />
+  </>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
